7b1777b738272ccfd525ca3da084b669dd9429f9
Mosho <moises.leon@chasacademy.se>
2026-02-02 07:33:28 +0100

    new branch for Routing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <a href="https://github.com/moooshooo/g-project" target="_blank">
         GitHub
       </a>
-      <p>G projektet | Kurs 2 - Ramverk & Komponentbaserad utveckling </p>
+      <p>Filmprojekt | Kurs 2 - Ramverk & Komponentbaserad utveckling </p>
       <p>Alexander & M. Leon </p>
       <p>Copy it's your right! 2026©</p>
     </footer>
