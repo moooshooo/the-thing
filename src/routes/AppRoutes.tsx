3d11571f07a2baedc3b3ec4d6c8d92a032a0445a
vyasso <89705636+vyasso@users.noreply.github.com>
2026-02-05 09:16:15 +0100

    adjusted login

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,7 +4,7 @@ import AboutPage from "../pages/AboutPage";
 import FilmPage from "../pages/FilmPage";
 import ShowPage from "../pages/ShowPages"
 import Login from "../pages/Login";
-
+  
 const AppRoutes = () => {
   return (
     <Routes>
@@ -12,12 +12,7 @@ const AppRoutes = () => {
       <Route path="/film" element={<FilmPage />}></Route>
       <Route path="/shows" element={<ShowPage />}></Route>
       <Route path="/about" element={<AboutPage />}></Route>
-<<<<<<< Updated upstream
-      <Route path="/login" element={<Login />} />
-    
-=======
       <Route path="/login" element={<Login />}></Route>
->>>>>>> Stashed changes
     </Routes>
   );
 };
